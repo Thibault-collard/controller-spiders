462e38db8d607b94c8fc67912bb8b10d651a5db0
Thibault Collard-bovy <thibault.collard@gmail.com>
2022-01-25 18:02:40 +0100

    solved problem key django

--- build/lib/run_spiders/settings.py
+++ build/lib/run_spiders/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-li5r!x0gle3q!@gwe4jku+=8pzbm3dp4#vn=fjkmsul5intz-l'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
