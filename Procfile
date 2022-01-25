4ef5359f6d4c0e31b22dd883ff0d9de2a8d1a983
Thibault Collard-bovy <thibault.collard@gmail.com>
2022-01-25 14:00:20 +0100

    modify wsgi launcher

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: gunicorn myproject.wsgi
+web: gunicorn run_spiders.wsgi
--- run_spiders/settings.py
+++ run_spiders/settings.py
@@ -9,7 +9,8 @@ https://docs.djangoproject.com/en/4.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
-
+# Configure Django App for Heroku.
+import django_heroku
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -122,3 +123,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+django_heroku.settings(locals())
